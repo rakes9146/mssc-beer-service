a6a56e001650eb549d119660407c551be3bc81eb
rakes9146 <rakeshsl546@gmail.com>
2025-02-02 13:15:29 +0530

    test fix

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package guru.springframework.msscbeerservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MsscBeerServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+	@Test
+	public void contextLoads() {
+	}
 }
