9ba59a885d88ad26116ef9fde9e9849c26deb746
rakes9146 <rakeshsl546@gmail.com>
2025-02-09 18:22:51 +0530

    fixing test. closes #11

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbeerservice.web.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,9 +14,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.request.*;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,7 +41,7 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
 
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto =  getBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMVc.perform(post("/api/v1/beer/")
@@ -50,13 +53,26 @@ class BeerControllerTest {
     @Test
     void updateBeerById() throws Exception {
 
-        BeerDto beerDto =  BeerDto.builder().build();
+        BeerDto beerDto =  getBeerDto();
         String beerDtoToJson = objectMapper.writeValueAsString(beerDto);
 
-        mockMVc.perform(post("/api/v1/beer/"+UUID.randomUUID().toString())
+        mockMVc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoToJson))
                 .andExpect(status().isNoContent());
 
     }
+
+    public BeerDto getBeerDto(){
+
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal(32.22))
+                .upc(22323243255L)
+                .build();
+
+        return beerDto;
+    }
+
 }
