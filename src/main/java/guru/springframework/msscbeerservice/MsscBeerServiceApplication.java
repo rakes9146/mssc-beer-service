ae6787cbeaa66f9e2a1961808789127f79919289
rakes9146 <rakeshsl546@gmail.com>
2025-03-15 12:32:07 +0530

    hiberanate error on startup with mysql. closes #26

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,12 +2,17 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class MsscBeerServiceApplication {
 
 	public static void main(String[] args) {
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "localmysql");
 		SpringApplication.run(MsscBeerServiceApplication.class, args);
+
+
 	}
 
 }
