140a301863b58cbdcc0d20f667ffcf6f6b2257db
rakes9146 <rakeshsl546@gmail.com>
2025-03-16 15:10:45 +0530

    creating beer event. closes #28

---
+++ src/main/java/guru/springframework/msscbeerservice/event/BeerEvent.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscbeerservice.event;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@RequiredArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = -234334343443L;
+
+    private final BeerDto beerDto;
+
+
+
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/event/BrewBeerEvent.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscbeerservice.event;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+
+public class BrewBeerEvent extends  BeerEvent{
+
+    public BrewBeerEvent(BeerDto beerDto) {
+        super(beerDto);
+    }
+}
